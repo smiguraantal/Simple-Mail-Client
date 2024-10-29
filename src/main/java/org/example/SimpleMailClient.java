948f686742f6254086a755d7b16e8c3ff0ae1cc8
smigu <smigura.antal@gmail.com>
2024-10-29 15:05:04 +0100

    added application.yaml

---
+++ src/main/java/org/example/SimpleMailClient.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMailClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleMailClient.class, args);
+    }
+}
